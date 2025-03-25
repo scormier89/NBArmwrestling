8f69a90684e0a969cb59c1504e18ea29717e2042
Stephane Cormier <stephane.cormier2@gmail.com>
2025-03-25 15:41:41 -0300

    capital case issue

--- src/router/index.js
+++ src/router/index.js
@@ -3,14 +3,14 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
 import ScheduleView from '@/views/ScheduleView.vue'
 import TeamsView from '@/views/TeamsView.vue'
-import CAWFView from '@/views/CAWFView.vue'
+import CawfView from '@/views/CawfView.vue'
 import ContactView from '@/views/ContactView.vue'
 
 const routes = [
   { path: '/', name: 'Home', component: HomeView },
   { path: '/schedule', name: 'Schedule', component: ScheduleView },
   { path: '/teams', name: 'Teams', component: TeamsView },
-  { path: '/cawf', name: 'CAWF', component: CAWFView },
+  { path: '/cawf', name: 'CAWF', component: CawfView },
   { path: '/contact', name: 'Contact', component: ContactView },
 ]
 
